a45ed8994f76728ec97d6f6e5daa5f998b43af51
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-04-15 11:11:00 -0300

    Add files via upload

---
+++ tarefa 2 avaliativas(2).c
@@ -0,0 +1,16 @@
+/*2. Efetuar a leitura de um número e apresentá-lo como o seu módulo (somente seu valor 
+absoluto) elaborando os cálculos matemáticos para isso.*/
+#include<stdio.h>
+int main(){
+	int numero;
+	printf("Digite o numero:\n");
+	scanf("%d", &numero);
+	if(numero < 0){
+		numero = -1 * numero;
+		printf("O modulo do numero e %d", numero);
+	}
+	else{
+		printf("O modulo do numero e %d", numero);
+	}
+	return 0;
+}
