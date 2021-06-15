996b83ee55f49217b7cdaace40333ddc5e7180b5
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-06-15 17:21:11 -0300

    Add files via upload

---
+++ atividade avaliativa 3 L(2).c
@@ -0,0 +1,20 @@
+/*3. Dado três valores, verificar se eles podem constituir os lados de um 
+triângulo (triângulo é umafigura geométrica onde cada 
+lado é menor do que a soma dos outros dois lados).*/
+
+#include<stdio.h>
+int main(){
+	double A, B, C;
+	printf("Digite o valor de A:\n");
+	scanf("%lf", &A);
+	printf("Digite o valor de B:\n");
+	scanf("%lf", &B);
+	printf("Digite valor de C:\n");
+	scanf("%lf", &C);
+	if(A + B <= C || A + C <= B || B + C <= A)
+	   printf("Impossivel constituir os lados de um triangulo!\n");
+	else{
+	    printf("E possivel constituir os lados de um triangulo!");
+	}
+	return 0;
+}
