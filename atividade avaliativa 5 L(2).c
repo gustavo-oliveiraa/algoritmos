a00ed26589166305c913716b125c37e3e646f150
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-06-15 17:41:02 -0300

    Add files via upload

---
+++ atividade avaliativa 5 L(2).c
@@ -0,0 +1,18 @@
+/*5. Complete os exercícios 1 (multa no eixão) e 3 (triângulo) com uma mensagem para quando a 
+condição não for satisfeita (falsa).*/
+#include<stdio.h>
+int main(){
+	double A, B, C;
+	printf("Digite o valor de A:\n");
+	scanf("%lf", &A);
+	printf("Digite o valor de B:\n");
+	scanf("%lf", &B);
+	printf("Digite valor de C:\n");
+	scanf("%lf", &C);
+	if(A + B <= C || A + C <= B || B + C <= A)
+	   printf("Impossivel constituir os lados de um triangulo!\n");
+	else{
+	    printf("E possivel constituir os lados de um triangulo!");
+	}
+	return 0;
+}
